57aff0d9c11cedfc816e635fbacd02025ffd6304
Thomas <1767941+tthomasgg@users.noreply.github.com>
2019-01-24 19:01:36 -0200

    removendo arq desnecessarios

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -11,10 +11,11 @@ import { Items } from '../items.model';
 })
 export class FormularioComponent implements OnInit {
   @ViewChild('f') formulario: NgForm;
-  /*  constructor(codigo?: number, nome: string, unidadeMedida: UnidadesMedida, quantidade: number, preco: number,
-              perecivel: boolean, dataValidade: string, dataFabricacao: string) {*/
-  quantidadePattern = "[0-9]";
+
+  quantidadePattern = '[0-9]';
+
   item: Items;
+
   codigo: number;
   nome: string;
   unidadeMedida: UnidadesMedida;
@@ -27,15 +28,15 @@ export class FormularioComponent implements OnInit {
   constructor(private itemsService: ItemsService, private router: Router, private route: ActivatedRoute) { }
 
   onUnidadeChange(event: any) {
-    switch(event.target["selectedIndex"]) {
-      //kilo
-      case 1 : this.quantidadePattern = "[0-9]+\.[0-9]{0,3}"; break;
-      //litro
-      case 2 : this.quantidadePattern = "[0-9]+\.[0-9]{0,3}"; break;
-      //unidade
-      case 3 : this.quantidadePattern = "[0-9]+"; break;
+    switch(event.target['selectedIndex']) {
+      // kilo
+      case 1 : this.quantidadePattern = '[0-9]{1,}\.{0,1}[0-9]{0,3}'; break;
+      // litro
+      case 2 : this.quantidadePattern = '[0-9]{1,}\.{0,1}[0-9]{0,3}'; break;
+      // unidade
+      case 3 : this.quantidadePattern = '[0-9]{1,}'; break;
       default :
-      this.quantidadePattern = "[0-9]";
+      this.quantidadePattern = '[0-9]';
     }
   }
 
@@ -46,7 +47,6 @@ export class FormularioComponent implements OnInit {
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
     this.item = this.itemsService.getItem(id);
-    //this.item = this.itemsService.getItem(id);
 
     if (typeof(this.item) !== 'undefined') {
       this.codigo = this.item.codigo;
@@ -55,49 +55,56 @@ export class FormularioComponent implements OnInit {
       this.quantidade = this.item.quantidade;
       this.preco = this.item.preco;
       this.cbPerecivel = this.item.perecivel;
-      this.dataValidade = new Date(this.item.dataValidade);
-      this.dataFabricacao = new Date(this.item.dataFabricacao);
+
+      const validade = this.item.dataValidade.split('/');
+      const validadeData = validade[1] + '-' + validade[0] + '-' + validade[2];
+
+      const fabricacao = this.item.dataFabricacao.split('/');
+      const fabricacaoData = fabricacao[1] + '-' + fabricacao[0] + '-' + fabricacao[2];
+
+      this.dataValidade = new Date(validadeData);
+      this.dataFabricacao = new Date(fabricacaoData);
     }
 
   }
 
   onSubmit() {
-    /*  constructor(codigo?: number, nome: string, unidadeMedida: UnidadesMedida, quantidade: number, preco: number,
-              perecivel: boolean, dataValidade: string, dataFabricacao: string) {*/
-                //console.log(this.formulario.value.validade);
-    let dataValidade = "";
-    let dataFabricacao = "";
-    let validade = this.formulario.value.userData.validade;
-    let fabricacao = this.formulario.value.userData.fabricacao;
+    let dataValidade = '';
+    let dataFabricacao = '';
+    const validade = this.formulario.value.userData.validade;
+    const fabricacao = this.formulario.value.userData.fabricacao;
 
     if (typeof(validade) === 'object') {
-      dataValidade = validade.getDate()  + "/" + (validade.getMonth()+1) + "/" + validade.getFullYear();
+      dataValidade = validade.getDate()  + '/' + (validade.getMonth() + 1) + '/' + validade.getFullYear();
     }
 
-    dataFabricacao = fabricacao.getDate()  + "/" + (fabricacao.getMonth()+1) + "/" + fabricacao.getFullYear();
+    dataFabricacao = fabricacao.getDate()  + '/' + (fabricacao.getMonth() + 1) + '/' + fabricacao.getFullYear();
 
-    if (typeof(this.formulario.value.userData.codigo) == 'undefined') {
+    if (typeof(this.formulario.value.userData.codigo) === 'undefined') {
       this.itemsService.addItem(
-                          new Items(null,
-                            this.formulario.value.userData.nome,
-                            this.formulario.value.userData.unidadeMedida,
-                            this.formulario.value.userData.quantidade,
-                            this.formulario.value.userData.preco,
-                            this.formulario.value.userData.perecivel,
-                            dataValidade,
-                            dataFabricacao
-                            ), true
+                          new Items(
+                              null,
+                              this.formulario.value.userData.nome,
+                              this.formulario.value.userData.unidadeMedida,
+                              this.formulario.value.userData.quantidade,
+                              this.formulario.value.userData.preco,
+                              this.formulario.value.userData.perecivel,
+                              dataValidade,
+                              dataFabricacao
+                            ),
+                            true
       );
     } else {
       this.itemsService.updateItem(
-                                new Items(this.formulario.value.userData.codigo,
-                                this.formulario.value.userData.nome,
-                                this.formulario.value.userData.unidadeMedida,
-                                this.formulario.value.userData.quantidade,
-                                this.formulario.value.userData.preco,
-                                this.formulario.value.userData.perecivel,
-                                dataValidade,
-                                dataFabricacao
+                                new Items(
+                                  this.formulario.value.userData.codigo,
+                                  this.formulario.value.userData.nome,
+                                  this.formulario.value.userData.unidadeMedida,
+                                  this.formulario.value.userData.quantidade,
+                                  this.formulario.value.userData.preco,
+                                  this.formulario.value.userData.perecivel,
+                                  dataValidade,
+                                  dataFabricacao
                                 )
         );
     }
